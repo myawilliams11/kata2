4bde7ffec521bf7df134530480858b4df8d0a380
Mya <35534673+myawilliams11@users.noreply.github.com>
2018-02-12 15:30:22 -0500

    Add files via upload

--- code.js
+++ code.js
@@ -1,31 +1,30 @@
-{const gotCitiesCSV = "King's Landing, Braavos, Volantis, Old Valyria, Free Cities, Qarth,Meereen"; 
-const cities= gotCitiesCSV.split(",")
-const div= document.createElement("div")
+var gotCitiesCSV = "King's Landing, Braavos, Volantis, Old Valyria, Free Cities, Qarth,Meereen"; 
+var cities= gotCitiesCSV.split(",")
+var div= document.createElement("div")
 // gives the quotes and brackets
 div.textContent= JSON.stringify(cities)
 document.body.appendChild(div)
-}
 
 
-{const bestThing = "The best thing about a boolean is even if you are wrong you are only off by a bit";
-const thing= bestThing.split( )
-const div= document.createElement("div")
+
+var bestThing = "The best thing about a boolean is even if you are wrong you are only off by a bit";
+var thing= bestThing.split( )
+var div= document.createElement("div")
 div.textContent= JSON.stringify(thing)
 document.body.appendChild(div)
-}
 
-{const gotCitiesCSV = "King's Landing, Braavos, Volantis, Old Valyria, Free Cities, Qarth,Meereen"; 
+
+var gotCitiesCSV = "King's Landing, Braavos, Volantis, Old Valyria, Free Cities, Qarth,Meereen"; 
 // why arent these semi-colons?!?!?!
-const cities= gotCitiesCSV.split(";;")
-const div= document.createElement("div")
+var cities= gotCitiesCSV.split(";;")
+var div= document.createElement("div")
 div.textContent= JSON.stringify(cities)
 document.body.appendChild(div)
-}
 
-{const lotrCitiesArray = "Mordor","Gondor","Rohan","Beleriand","Mirkwood","Dead Marshes","Rhun","Harad"; 
-const cities= gotCitiesCSV.split(",")
-const div= document.createElement("div")
+
+var lotrCitiesArray = ["Mordor","Gondor","Rohan","Beleriand","Mirkwood","Dead Marshes","Rhun","Harad"];
+var array= lotrCitiesArray.join(",")
+var div= document.createElement("div")
 // gives the quotes and brackets
-div.textContent= JSON.stringify(cities)
+div.textContent= JSON.stringify(array)
 document.body.appendChild(div)
-}
